0da63aa05361861795514f49976bd424aadbed6d
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2026-01-15 11:18:42 -0300

    Adicionando novo projeto Ecofy ao portfólio

--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -7,6 +7,7 @@ import LogoNext from '/logo-next.png';
 import LogoPostgres from '/logo-postgres.png';
 import LogoNode from '/logo-node.png';
 import LogoExpress from '/logo-express.png';
+import LogoCloudinary from '/logo-cloudinary.png';
 import LogoMongodb from '/logo-mongodb.png';
 import LogoGit from '/logo-git.png';
 import LogoFigma from '/logo-figma.png';
@@ -25,7 +26,15 @@ const Projects = ({ id }) => {
         />
 
         <Project
-            nome="Clone Spotify"
+            nome="Ecofy"
+            foto="/foto-ecofy.png"
+            descricao="O Ecofy funciona como um catálogo full-stack, onde o usuário terá libardade de utilizar diversas funcionalidades para cadastrar, visualizar, atualizar ou deletar informações de plantas que estão cadastradas em um banco de dados, além disso, sua criação foi feita para aplicar conhecimentos e também torna-lo base para outros projetos maiores que virão no futuro. Esse projeto possui front-end e back-end separados, desenvolvidos em ReactJS e Node.js respectivamente, mas conectados juntamente com um banco de dados MongoDB, buscando sempre utilizar boas práticas de desenvolvimento, assim como tecnologias modernas e importantes."
+            linkRepositorio="https://github.com/Walmir07/ecofy_web"
+            iconesTec={[LogoJavaScript, LogoReact, LogoNode, LogoExpress, LogoCloudinary, LogoMongodb, LogoGit, LogoFigma]}
+        />
+
+        <Project
+            nome="Spotify - Clone"
             foto="/foto-spotify-clone.jpeg"
             descricao="Este projeto de clone teve como objetivo recriar a experiência do Spotify, utilizando diversas tecnologias modernas e integração com banco de dados. Esse sistema permite que os usuários busquem músicas e artistas, reproduzam faixas e desfrutem de uma experiência imersiva. Desenvolvido durante a Jornada FullStack da Hashtag Treinamentos, este projeto foi muito importante por ampliar meus conhecimentos em desenvolvimento FullStack."
             linkRepositorio="https://github.com/Walmir07/projeto-spotify-fullstack"
