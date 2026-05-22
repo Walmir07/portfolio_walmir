64dc80fad8e443a8b0cd17778ecceba5c4bf0b31
Walmir07 <walmirlima.dev@gmail.com>
2026-05-22 08:59:52 -0300

    Padronizando textos do footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='footer'>
         <p className='texto-footer'>Desenvolvido por Walmir Lima | Todos os direitos reservados      
         </p>
-        <div> Icons made by <a href="https://www.magnific.com" title="Freepik"> Freepik </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com'</a></div>
+        <div className='texto-footer'> Icons made by <a href="https://www.magnific.com" title="Freepik"> Freepik </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com'</a></div>
     </div>
   )
 }
